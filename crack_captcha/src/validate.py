0c4c42928a4097c3a0563e4f170dc3b532831ade
Anye <Anye@users.noreply.github.com>
2018-07-26 18:12:47 +0800

    支持 check 多个，验证真实准确率

--- crack_captcha/src/validate.py
+++ crack_captcha/src/validate.py
@@ -5,8 +5,9 @@ import config
 import numpy as np
 from gen_image import convert2gray, gen_random_captcha_image, array_to_text
 
-
-def crack_captcha(captcha_image):
+if __name__ == '__main__':
+    test_count = 100
+    right_count = 0
     x_input = tf.placeholder(tf.float32, [None, config.IMAGE_HEIGHT * config.IMAGE_WIDTH])
     keep_prob = tf.placeholder(tf.float32)  # dropout
     output = create_layer(x_input, keep_prob)
@@ -15,29 +16,25 @@ def crack_captcha(captcha_image):
     with tf.Session() as sess:
         saver.restore(sess, tf.train.latest_checkpoint('.'))
         predict = tf.argmax(tf.reshape(output, [-1, config.MAX_CAPTCHA, config.CHAR_SET_LEN]), 2)
-        text_list = sess.run(predict, feed_dict={x_input: [captcha_image], keep_prob: 1})
 
-        text = text_list[0].tolist()
-        vector = np.zeros(config.MAX_CAPTCHA * config.CHAR_SET_LEN)
-        i = 0
-        for n in text:
-            vector[i * config.CHAR_SET_LEN + n] = 1
-            i += 1
+        for x in range(0, test_count):
+            label_text, captcha_image = gen_random_captcha_image()
+            captcha_image = convert2gray(captcha_image)
+            captcha_image = captcha_image.flatten() / 255
+            text_list = sess.run(predict, feed_dict={x_input: [captcha_image], keep_prob: 1})
 
-        return array_to_text(vector)
+            text = text_list[0].tolist()
+            vector = np.zeros(config.MAX_CAPTCHA * config.CHAR_SET_LEN)
+            i = 0
+            for n in text:
+                vector[i * config.CHAR_SET_LEN + n] = 1
+                i += 1
+            predict_text = array_to_text(vector)
+            if predict_text == label_text:
+                right_count = right_count + 1
+                result = 'y'
+            else:
+                result = 'n'
+            print("label is : {} <----> predict is : {} result: {}".format(label_text, predict_text, result))
 
-
-def validate_image():
-    text, image = gen_random_captcha_image()
-    image = convert2gray(image)
-
-    # map the value to 0 -> 1 ，this will really affect the loss function update ,always remember the value should
-    # suit to the loss learning rate
-    # refer https://www.youtube.com/watch?v=pU5TG_X7b6E&index=6&list=PLwY2GJhAPWRcZxxVFpNhhfivuW0kX15yG 21:00
-    image = image.flatten() / 255
-    predict_text = crack_captcha(image)
-    print("label is : {} <----> predict is : {}".format(text, predict_text))
-
-
-if __name__ == '__main__':
-    validate_image()
+        print("all result %s/%s , accuracy: %s" % (right_count, test_count, right_count/test_count))
